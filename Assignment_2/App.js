cb31a7e9c553753c49a58fb35dab3230f24cf299
sameersheikh2 <sameersheikh819@gmail.com>
2023-01-06 14:16:48 +0530

    install parcel

--- Assignment_2/App.js
+++ Assignment_2/App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading1 = React.createElement("h1", {}, "this is heading 1"); //nested react element
 const heading2 = React.createElement("h2", {}, "this is heading 2");
 const container = React.createElement("div", {}, [heading1, heading2]);
