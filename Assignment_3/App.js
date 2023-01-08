a9df26324e6ff4840d6e67a6fe427a09c4ae8c17
sameersheikh2 <sameersheikh819@gmail.com>
2023-01-08 19:19:11 +0530

    create element using JSX

--- Assignment_3/App.js
+++ Assignment_3/App.js
@@ -7,5 +7,13 @@ const container = React.createElement("div", { className: "title" }, [
   React.createElement("h3", {}, "this is heading 3"),
 ]);
 
+const container2 = (
+  <div className="title2">
+    <h1>This is heading 1 </h1>
+    <h2> This is heading 2</h2>
+    <h3>This is heading 3</h3>
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+root.render({ container2 });
