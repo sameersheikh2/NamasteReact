c6b6e63382f06b389ff9e7dca7654356a0c92976
sameersheikh2 <sameersheikh819@gmail.com>
2023-01-11 18:41:32 +0530

    add static restaurant data

--- Assignment_4/App.js
+++ Assignment_4/App.js
@@ -16,10 +16,40 @@ const Header = () => {
   );
 };
 
+const RestaurantList = (props) => {
+  return (
+    <div className="restaurant_list">
+      <img
+        src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/osw1nuhqh5w7st158hq2"
+        width="200px"
+        alt="img"
+      />
+      <h2>Subway</h2>
+      <h3>Salads, Snacks</h3>
+      <h4>26 mins</h4>
+    </div>
+  );
+};
+const RestaurantCard = () => {
+  return (
+    <div className="card">
+      <RestaurantList />
+      <RestaurantList />
+      <RestaurantList />
+      <RestaurantList />
+      <RestaurantList />
+    </div>
+  );
+};
+const Body = () => {
+  return <RestaurantCard />;
+};
+
 const AppLayout = () => {
   return (
     <>
       <Header />
+      <Body />
     </>
   );
 };
