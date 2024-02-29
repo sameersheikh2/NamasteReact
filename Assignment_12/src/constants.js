267674e45987289741f9fcc5edeba0d92d1c955e
Sameer Sheikh <56081152+sameersheikh2@users.noreply.github.com>
2024-02-29 17:32:26 +0530

    Update constants.js

--- Assignment_12/src/constants.js
+++ Assignment_12/src/constants.js
@@ -2,7 +2,7 @@ export const IMG_CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
 export const ALL_RESTAURANT =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&sortBy=RELEVANCE&page_type=DESKTOP_WEB_LISTING";
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
 export const RESTAURANT_SORTBY_DELIVERY =
   "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&sortBy=DELIVERY_TIME&page_type=DESKTOP_WEB_LISTING";
