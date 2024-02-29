67396c5915efc2310ae4acc8ea4fadc4c1b0f204
Sameer Sheikh <56081152+sameersheikh2@users.noreply.github.com>
2024-02-29 17:27:35 +0530

    Update useRestaurant.js

--- Assignment_12/src/utils/useRestaurant.js
+++ Assignment_12/src/utils/useRestaurant.js
@@ -14,8 +14,8 @@ const useRestaurant = () => {
         const res = await fetch(fetchRestaurantBy);
         const data = await res.json();
         if (fetchRestaurantBy === ALL_RESTAURANT) {
-          setFilteredRestaurants(data.data?.cards[2]?.data?.data?.cards);
-          setAllRestaurants(data.data?.cards[2]?.data?.data?.cards);
+          setFilteredRestaurants(data.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+          setAllRestaurants(data.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
         } else {
           setFilteredRestaurants(data.data?.cards[0]?.data?.data?.cards);
           setAllRestaurants(data.data?.cards[0]?.data?.data?.cards);
